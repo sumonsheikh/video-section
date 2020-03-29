b3adfe078b1a5fe50bcd7ecc85329ba87726084f
sumonsheikh <sm.sumon117@gmail.com>
2020-03-29 22:54:33 +0600

    all is ok

---
+++ js/main.js
@@ -0,0 +1,11 @@
+(function($){
+    jQuery(document).ready(function($){
+        $(".video-play-btn").magnificPopup({
+         type:'video', 
+
+        });
+    });
+    jQuery(window).load(function(){
+
+    });
+}(jQuery));
